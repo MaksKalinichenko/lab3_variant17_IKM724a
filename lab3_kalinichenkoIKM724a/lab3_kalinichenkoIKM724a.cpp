e099f97298e189493e17bce6d4942d3928f9cf95
user <user@WIN-TC71ID3OSPT>
2024-12-06 12:15:20 +0200

    Добавьте файлы проекта.

---
+++ lab3_kalinichenkoIKM724a/lab3_kalinichenkoIKM724a.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm> // для std::min
+
+int main() {
+    setlocale(LC_CTYPE, "ukr"); // дозволити використання української мови
+
+    // Заданные значения
+    double r = 0.7; // Радиус шара
+    double a = 2.0; // Сторона a
+    double b = 1.5; // Сторона b
+
+    // Обчисляем диаметр шара
+    double diameter = 2 * r;
+
+    // Проверяем, может ли шар пройти через отверстие
+    if (diameter <= std::min(a, b)) {
+        std::cout << "Куля пройде через отвір." << std::endl;
+    }
+    else {
+        std::cout << "Куля НЕ пройде через отвір." << std::endl;
+    }
+
+    return 0;
+}
